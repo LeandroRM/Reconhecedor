9962b8ff3c5164ae5c2945f68c8d33f102d202f2
Leandro Ramos Marcelino <leandroramosmarcelino@hotmail.com>
2016-10-13 10:22:06 -0300

    Iniciando repositorio

---
+++ reconhecedor.py
@@ -0,0 +1,18 @@
+comandos = []
+variaveis = []
+
+nomeArquivo = input('Nome do arquivo: ')
+
+arquivo = open(nomeArquivo, 'r')
+
+for line in arquivo.readLines():
+    # Quebra por espaço
+    var palavras = line.split()
+    
+    # Aqui vão ter que ser seguidas certas regras:
+    #   1. 
+    #
+    #
+    #
+    #
+    for word in palavras:
