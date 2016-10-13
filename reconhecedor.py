4159ef1ceadeeabfc2d5fe4fc046d6a0a18c05ee
Leandro Ramos Marcelino <leandroramosmarcelino@hotmail.com>
2016-10-13 11:10:46 -0300

    Estruturado forma de funcionamento do script

--- reconhecedor.py
+++ reconhecedor.py
@@ -1,18 +1,42 @@
-comandos = []
-variaveis = []
+#Fluxo principal de código
+def main:
+    comandos = []
+    variaveis = []
 
-nomeArquivo = input('Nome do arquivo: ')
+    nomeArquivo = input('Nome do arquivo: ')
 
-arquivo = open(nomeArquivo, 'r')
+    arquivo = open(nomeArquivo, 'r')
 
-for line in arquivo.readLines():
-    # Quebra por espaço
-    var palavras = line.split()
+    for line in arquivo.readLines():
+        # Cada frase é enviada para o reconhecedor
+        # De acordo com o retorno do reconhecedor será decido qual ação tomar em seguida
+        retorno = reconhecer(line)
+
+
+# Função responsável por interpretar a frase
+# 1.Reconhecer palavras-chaves/reservadas
+# 2.Reconhecer Strings
+# 3.Reconhecer variáveis
+# 4.Retornar valor a ser armazenado no array COMANDOS
+def reconhecer:
     
-    # Aqui vão ter que ser seguidas certas regras:
-    #   1. 
-    #
-    #
-    #
-    #
-    for word in palavras:
+
+def reconhecerWrite:
+
+
+def reconhecerRead:
+
+
+def reconhecerVar:
+
+
+def reconhecerIf:
+
+
+def reconhecerFor:
+
+
+# Função responsável por executar os comandos
+# Interpretar a linha e executar o comando respectivo
+# Por enquanto esse método só vai verificar que comando foi encontrado ou se aconteceu erro;
+def executar:
